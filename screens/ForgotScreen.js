f66c5cf605dc35d465b22373767ab811be8df1aa
Aditya <ar1364713@gmail.com>
2023-11-22 15:08:13 +0530

    Rest Password functionality Added

--- screens/ForgotScreen.js
+++ screens/ForgotScreen.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, Text, StatusBar, Image, Dimensions, TouchableOpacity } from 'react-native'
 import MaterialTextInput from '../components/MaterialTextInput'
 import MaterialButton from '../components/MaterialButton'
 import { useNavigation } from '@react-navigation/native';
+import LoadingModal from '../components/LoadingModal';
+import auth from '@react-native-firebase/auth'
 
 const ForgotScreen = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+
+  function isValidEmail(email) {
+    // Regular expression for basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  const resetPassword = async() => {
+    if(email===''){
+      alert('please enter your email')
+    }
+    else if(isValidEmail(email)){
+      setLoading(true)
+      try {
+        await auth().sendPasswordResetEmail(email).catch((error)=>{
+          setLoading(false)
+          alert(error.message);
+          setLoading(false)
+        }).then(()=>{
+          setLoading(false)
+          alert('Check Your Email! Reset Link Sent')
+
+        })
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        alert(error.message)
+      }
+    }
+    else{
+      alert('Enter a valid email')
+    }
+  }
   return (
     <View  style={styles.container}>
       <View style={styles.imageContainer}>
@@ -15,9 +52,9 @@ const ForgotScreen = () => {
       <Text style={[styles.title, {color: 'gray', fontWeight: 'normal', fontSize: 18}]}>Forgot Password?</Text>
         <Text style={styles.title}>Let's<Text style={{ color: '#FF5A66' }}> Reset</Text> It</Text>
         <View style={{marginTop: 30}}>
-        <MaterialTextInput placeholder='Email' />
+        <MaterialTextInput value={email} onChangeText={setEmail} placeholder='Email' />
         <View style={{marginVertical: 20}}>
-        <MaterialButton backgroundColor={'black'} text={'Send Reset Link'} height={50} borderRadius={20} isLoading={false} indicatorColor={'#fff'}  />
+        <MaterialButton onPress={resetPassword} backgroundColor={'black'} text={'Send Reset Link'} height={50} borderRadius={20} isLoading={loading} indicatorColor={'#fff'}  />
         <TouchableOpacity onPress={()=>navigation.goBack()}>
         <Text style={styles.rText}>Password Remembered? <Text style={{color: 'royalblue'}}>Go back</Text></Text>
         </TouchableOpacity>
@@ -25,6 +62,7 @@ const ForgotScreen = () => {
         </View>
       </View>
       <StatusBar backgroundColor={'lightgray'} barStyle={'dark-content'} />
+      <LoadingModal isVisible={loading} />
     </View>
   )
 }
